264c7c780754bcd2b4e35e4fccde2dbe9ade8493
Sven Mol <s.mol@student.utwente.nl>
2020-01-19 00:56:59 +0100

    Remove redundant lines in stringCommons

--- build/stringCommons.ts
+++ build/stringCommons.ts
@@ -1,8 +1,6 @@
-export const capitalize: StringMapper = (value: string): string => {
-	return (
-		value.charAt(0).toUpperCase() +
-		value.substring(1)
-	)
-};
+export const capitalize: StringMapper = (value: string): string => (
+	value.charAt(0).toUpperCase() +
+	value.substring(1)
+);
 
 export type StringMapper = (v: string) => string;
